4e0c61079e69ba78b9b176892a18b824673b7c87
Michael Pangburn <michaelpangburn@comcast.net>
2018-04-09 23:46:30 -0700

    Initial commit

---
+++ FunctionKit/FunctionKit.h
@@ -0,0 +1,18 @@
+//
+//  FunctionKit.h
+//  FunctionKit
+//
+//  Created by Michael Pangburn on 4/9/18.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FunctionKit.
+FOUNDATION_EXPORT double FunctionKitVersionNumber;
+
+//! Project version string for FunctionKit.
+FOUNDATION_EXPORT const unsigned char FunctionKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FunctionKit/PublicHeader.h>
+
+
---
+++ FunctionKitTests/FunctionKitTests.swift
@@ -0,0 +1,14 @@
+//
+//  FunctionKitTests.swift
+//  FunctionKitTests
+//
+//  Created by Michael Pangburn on 4/9/18.
+//
+
+import XCTest
+@testable import FunctionKit
+
+
+class FunctionKitTests: XCTestCase {
+    
+}
