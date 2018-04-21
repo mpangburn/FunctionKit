390c8dfc94543611098d9ba6063966611dfad9ab
Michael Pangburn <michaelpangburn@comcast.net>
2018-04-20 18:46:35 -0700

    Add Package.swift

---
+++ Package.swift
@@ -0,0 +1,17 @@
+// swift-tools-version:4.0
+
+import PackageDescription
+
+let package = Package(
+    name: "FunctionKit",
+    products: [
+        .library(name: "FunctionKit", targets: ["FunctionKit"])
+    ],
+    targets: [
+        .target(
+            name: "FunctionKit",
+            dependencies: []
+        )
+    ]
+)
+  
